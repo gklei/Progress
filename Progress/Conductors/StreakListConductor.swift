57808fcb6baf17dbd187be078dcdded730ca1bb7
gklei <gregory@incipia.co>
2018-05-18 17:45:20 -0700

    Added new images and files.

---
+++ Progress/Conductors/StreakListConductor.swift
@@ -0,0 +1,9 @@
+//
+//  StreakListConductor.swift
+//  Streaks
+//
+//  Created by Gregory Klein on 5/18/18.
+//  Copyright © 2018 Gregory Klein. All rights reserved.
+//
+
+import Foundation
---
+++ Progress/View Controllers/StreakListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  StreakListViewController.swift
+//  Streaks
+//
+//  Created by Gregory Klein on 5/18/18.
+//  Copyright © 2018 Gregory Klein. All rights reserved.
+//
+
+import Foundation
