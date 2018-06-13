ed1d212b65f27f0df547279f35f1bef83668d115
gklei <gregory@incipia.co>
2018-06-13 00:14:29 -0400

    Cleanup.

--- Progress/Extensions/UITabBar+Streaks.swift
+++ Progress/Extensions/UITabBar+Streaks.swift
@@ -14,20 +14,21 @@ extension UITabBar {
       tintColor = .white
       isTranslucent = false
       backgroundImage = UIImage.with(color: .chalkboard)
-      
+      setShadow(opacity: 0.5, xOffset: 0, yOffset: 2, radius: 20)
+   }
+   
+   private func _shadowImage(lColor: ProgressColor, rColor: ProgressColor) -> UIImage {
       let gradientLayer = CAGradientLayer()
       gradientLayer.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 2)
       gradientLayer.colors = [UIColor(.lipstick).cgColor, UIColor(.pink).cgColor]
       gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
       gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
-
+      
       UIGraphicsBeginImageContext(gradientLayer.bounds.size)
       gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
       let image = UIGraphicsGetImageFromCurrentImageContext()!
       UIGraphicsEndImageContext()
       
-//      shadowImage = UIImage.with(color: .mint)
-//      shadowImage = image
-      setShadow(opacity: 0.5, xOffset: 0, yOffset: 2, radius: 20)
+      return image
    }
 }
